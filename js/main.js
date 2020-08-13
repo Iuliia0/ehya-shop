cadbc8bc6bf0687fa3da09394d313790002a9e0c
Iuliia0 <julia03951@gmail.com>
2020-08-14 00:54:24 +0300

    create: sleder & responsive for testimonial

--- js/main.js
+++ js/main.js
@@ -1,12 +1,26 @@
 $(document).ready(function () {
-  var tabsItem = $('.trending__item');
-  var contentItem = $('.trending-ware');
+  var tabsItem = $(".trending__item");
+  var contentItem = $(".trending-ware");
 
-  tabsItem.on('click', function (event) {
-    var activeContent = $(this).attr('data-target');
-    tabsItem.removeClass('trending__item--active');
-    contentItem.removeClass('trending-ware--active');
-    $(activeContent).addClass('trending-ware--active');
-    $(this).addClass('trending__item--active');
+  tabsItem.on("click", function (event) {
+    var activeContent = $(this).attr("data-target");
+    tabsItem.removeClass("trending__item--active");
+    contentItem.removeClass("trending-ware--active");
+    $(activeContent).addClass("trending-ware--active");
+    $(this).addClass("trending__item--active");
+  });
+
+  var testimonialSlider = new Swiper(".testimonial-slider", {
+    autoHeight: true,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      dynamicMainBullets: 3,
+    },
+    loop: true,
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
   });
 });
